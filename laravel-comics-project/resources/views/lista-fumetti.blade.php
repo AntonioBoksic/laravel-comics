e8f09744bd78af36e1556cfb549319a3477c9a02
Antonio <antonioboksic1603@gmail.com>
2023-07-12 00:41:22 +0200

    aggiunto dati e stampato card con foreach con successo. Bisognerebbe dare un po di stile ma vabbè. Per quanto riguarda il bonus, al posto della view "lista-fumetti" si può tornare alla view "home" sempre all'interno dello stesso layout cliccando sul link apposito.

--- laravel-comics-project/resources/views/lista-fumetti.blade.php
+++ laravel-comics-project/resources/views/lista-fumetti.blade.php
@@ -3,12 +3,33 @@
 @section('content')
 
 <div>
-<h1>
-            QUESTA è LA PAGINA LISTA-FUMETTI.BLADE.PHP
 
-            <br>
-            <a href="{{route('home')}}">HOME</a>
-        </h1>
+    QUESTA è LA PAGINA LISTA-FUMETTI.BLADE.PHP
+    <br>
+    <a href="{{route('home')}}">HOME</a>
+
+    <div>
+        <h2 class="mt-5">Lista fumetti:</h2>
+
+        <div class="d-flex flex-wrap">
+            
+            @foreach($fumetti as $fumetto)
+            <div class="card my-2 col-2 ">
+
+                <div class="immagine-fumetto">
+                <img src="{{$fumetto['thumb']}}" alt="??immagine fumetto??">
+                </div>
+                
+                <div>
+                {{$fumetto['title']}}
+                </div>
+                
+            </div>
+            @endforeach
+        </div>
+        
+    </div>
+        
 </div>
 
 @endsection
